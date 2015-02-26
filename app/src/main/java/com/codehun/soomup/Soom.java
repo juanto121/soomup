633951e8247b983766e8978775205b15e2b38c40
Juan Torres <juanto121@hotmail.com>
2015-02-25 23:50:34 -0500

    Start ripple working... Where is the break!!

--- app/src/main/java/com/codehun/soomup/Soom.java
+++ app/src/main/java/com/codehun/soomup/Soom.java
@@ -4,16 +4,27 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.skyfishjy.library.RippleBackground;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Soom extends Activity implements AnswerFragment.AnswerListSelectionListener {
 
+    private static String TAG = "SoomActivity";
+    private RippleBackground ripple_back;
     private Game game;
     private FragmentManager fragments;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,7 @@ public class Soom extends Activity implements AnswerFragment.AnswerListSelection
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soom);
 
+        ripple_back =  (RippleBackground) findViewById(R.id.content);
         fragments = getFragmentManager();
 
         AnswerFragment answers = (AnswerFragment) fragments.findFragmentById(R.id.answer_fragment);
@@ -49,18 +61,18 @@ public class Soom extends Activity implements AnswerFragment.AnswerListSelection
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
+        }
 
     @Override
     public void onAnswerSelection(int index) {
-
+        Log.d(TAG, "Item selected in answer list view");
+        ripple_back.startRippleAnimation();
     }
 
     public Game getGame(){
